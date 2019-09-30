96f87078494b6b26821af1f2de6a455f48779f19
mupotsal <46546491+mupotsal@users.noreply.github.com>
2019-09-29 23:05:53 -0400

    edited the flip method

--- parity/parity-main.cpp
+++ parity/parity-main.cpp
@@ -1,6 +1,6 @@
 /** L05: Parity
-FIXME: Liberty Mupotsa 
-FIXME: username(s): mupotsal
+: Liberty Mupotsa 
+: username(s): mupotsal
 Purpose: To use arrays, classes, and to learn some things about binary.
 
 Acknowledgements:
@@ -110,17 +110,21 @@ void flip() {
 	for (int i = 0; i < bytelen; i++) {
 		if (byte[i] == 1) {
 			byte[i] = 0;
+			cout << byte[i];
 		}
 		else {
 			byte[i] = 1;
+			cout << byte[i];
 		}
-
+		//return byte; if you want the value but do not want to see it one just uncomment this
 	}
 	/*for (int i = 0; i < bytelen; i++) { //  put this to test if my method is working
 		cout << byte[i];
-	}*/
-}		
 
+	}*/
+}
+
+// The following returns a byte to the main.
 bool* byte_array() {
 
 	bool static bit_array[8];
@@ -130,21 +134,23 @@ bool* byte_array() {
 	return bit_array;
 }
 
-bool masking(bool maskbyte[]) {
+// The function masks the given byte
+void  masking(bool maskbyte[]) {
 	for (int i=0;i < bytelen; i++)
 		if (byte[i] && maskbyte[i] == 1) {
-			byte[i] = true;
+			 byte[i] = true;
+			cout << byte[i];
 		}
 		else {
 			byte[i] = 0;
+			cout << byte[i];
 		}
+	
+	return;
 
-	return byte;
 }
  
 friend ostream& operator <<(ostream& stream,  Parity& par);
-
-
 
 private:
 	bool byte[8] = { false }; //This initializes all of the bits
@@ -170,49 +176,59 @@ int main() {
 	bool truebyte[8] = { true, true, true, true, true, true, true, true}; // this style of initalization only works at compile time
 	int readchar = 'Y';
 
-	cout << "Welcome to the parity class!\n" << endl; //FIXME: Feel free to change this!
+	cout << "Welcome to the parity class Have fun!\n" << endl; //FIXME: Feel free to change this!
 
 	Parity testbyte;
-	Parity mybyte(truebyte);  // uses contructor that sets byte using parameter
-
-
-
-
-	cout << "Flip the array" << endl;
-	//testbyte.flip();
-	//testbyte.flip() ;
+	Parity mybyte(truebyte);  // uses contructor that sets byte using parameter	
+	
 
 	testbyte.set_byte();
 	cout << "Here is the byte you entered: ";
 	testbyte.show_byte();
-	bool maskbyte[8] = { true, true, false, true, true, true, true, true };
-	testbyte.masking(maskbyte);
+
+
+	cout << "Is mybyte_odd__ " << mybyte.is_odd() << endl;
+	cout << "Is testbyte odd__" << testbyte.is_odd() << endl;
+
+
+	bool maskbyte[8] = { true, true, true, true, false, true, true, true};
+	cout << "        " << endl;
+	cout << "This is the masked byte(makebyte masked with the entered byte)" << endl;	testbyte.masking(maskbyte);
+	cout << "        " << endl;
+	cout << "        " << endl;
+	
+	//cout << endl;
+	cout << "Mybyte flipped" << endl;
+	mybyte.flip();
+	
+
+
+	cout <<"This is the testbyte printed ussing << operator "<<testbyte << endl;// overloaded the << operator to print  the byte using <<
 
 
 	
-	cout << endl;
 
-	cout << testbyte << endl;
-	cout << "Here is anthor byte: " << endl;	
-	mybyte.show_byte();
 
-	cout << "Is it odd__ " << mybyte.is_odd() << endl;
-	cout << "Is testbyte odd__" << testbyte.is_odd() << endl;
 	
 	// Testing set even and is working
-	testbyte.set_even();
-	cout << "The mybyte is set to even below " << endl;
-	mybyte.set_even();
-
+	cout << "        " << endl;
 	
-
-	cout << "The mybyte is set to odd below " << endl;
+	testbyte.set_even();
+	cout << "The testbyte is set to even below " << endl;
+	
+	cout << "        " << endl;
+	cout << "mybyte set even" << endl;
+	mybyte.set_even();
+	
+	cout << "        " << endl;
+	cout << "        " << endl;
+	cout << "The twstbyte is set to odd below " << endl;
 	testbyte.set_odd();
-	cout << "This is the printed one" << endl;
-	cout  << testbyte<< endl;
-
+	cout << "The mybyte is set to odd below " << endl;
 	mybyte.set_odd();
 
+	cout << "This is the printed one" << endl;
+	cout  << testbyte<< endl;
 	
 
 	cout << "Flip the array" << endl;
@@ -222,16 +238,12 @@ int main() {
 	bool *get_array; // using pointers to  get the array values stored
 	cout << "The following is the byte returned " << endl;
 	get_array = mybyte.byte_array();
-
-	/*for (int i = 0; i < 8; i++) {
-		cout << "*(get_array + " << i << ") : ";
-		cout << *(get_array + i) << endl;
-	}*/
-	cout << get_array << endl;
+	
+	
 	
 	//cout << tbyte << endl;
 	cout << endl;
-	//FIXME: place code to fully test your Parity class
+	
 
 	
 
